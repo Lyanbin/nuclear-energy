11c334b39f39998081ba8b17244dca8cf910228f
Lyanbin <champagne.lyb@gmail.com>
2019-10-21 20:46:50 +0800

    feat: add gitignore

---
+++ build/precache-manifest.82f210f4f5a51d0e211bc84856eb2959.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee04304b6c057d576c0de78bc79f4ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aa421795f872400d78d",
+    "url": "/static/css/main.c68af30a.chunk.css"
+  },
+  {
+    "revision": "afdce02396f611c80884",
+    "url": "/static/js/2.c22136ea.chunk.js"
+  },
+  {
+    "revision": "0aa421795f872400d78d",
+    "url": "/static/js/main.172320d3.chunk.js"
+  },
+  {
+    "revision": "ec154ca6f89d095cbf9c",
+    "url": "/static/js/runtime-main.8fdb0740.js"
+  },
+  {
+    "revision": "f2186e7734092848ca3c4da8c77c2632",
+    "url": "/static/media/fuck.f2186e77.jpg"
+  },
+  {
+    "revision": "11c8d5a8efb7ba791c9e474fdd5c8dad",
+    "url": "/static/media/fuckwithstick.11c8d5a8.jpg"
+  },
+  {
+    "revision": "283ce43b3ce74d1c7318ce2f6979856d",
+    "url": "/static/media/really.283ce43b.jpg"
+  }
+]);
